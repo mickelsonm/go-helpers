f164efa2ee018a1b88cc5c2257a87136c357f8d3
Matt Mickelson <mickelmr@gmail.com>
2015-01-11 20:16:00 -0600

    adds encryption helper

---
+++ encryption/README.md
@@ -0,0 +1,35 @@
+Encryption Helper
+==========
+
+This helper is setup to handle various forms of encryption and decryption mechanisms out there.
+
+AES Ecryption Example:
+```go
+	package main
+
+	import (
+		"fmt"
+		"github.com/mickelsonm/go-helpers/encryption"
+	)
+
+	func main() {
+		//Key needs to have a length of 16, 24, or 32 bytes to select
+		//AES-128, AES-192, or AES-256 (per Golang documentation).
+		CIPHER_KEY := []byte("DecryptMagic2015")
+		msg := "This is a top secret message."
+
+		encrypted, err := encryption.AES_Encrypt(CIPHER_KEY, msg)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Printf("Encrypted: %s\n", encrypted)
+
+		decrypted, err := encryption.AES_Decrypt(CIPHER_KEY, encrypted)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Printf("Decrypted: %s\n", decrypted)
+	}
+```
---
+++ encryption/aes.go
@@ -0,0 +1,65 @@
+package encryption
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"encoding/base64"
+	"errors"
+	"io"
+)
+
+func AES_Encrypt(key []byte, msg string) (encoded_msg string, err error) {
+	plainText := []byte(msg)
+
+	//Key needs to have a length of 16, 24, or 32 bytes to select
+	//AES-128, AES-192, or AES-256 (per Golang documentation).
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return
+	}
+
+	//IV needs to be unique, but doesn't have to be secure.
+	//It's common to put it at the beginning of the ciphertext.
+	cipherText := make([]byte, aes.BlockSize+len(plainText))
+	iv := cipherText[:aes.BlockSize]
+	if _, err = io.ReadFull(rand.Reader, iv); err != nil {
+		return
+	}
+
+	stream := cipher.NewCFBEncrypter(block, iv)
+	stream.XORKeyStream(cipherText[aes.BlockSize:], plainText)
+
+	//returns to base64 encoded string
+	encoded_msg = base64.URLEncoding.EncodeToString(cipherText)
+	return
+}
+
+func AES_Decrypt(key []byte, secure_msg string) (decoded_msg string, err error) {
+	cipherText, err := base64.URLEncoding.DecodeString(secure_msg)
+	if err != nil {
+		return
+	}
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return
+	}
+
+	if len(cipherText) < aes.BlockSize {
+		err = errors.New("Ciphertext block size is too short!")
+		return
+	}
+
+	//IV needs to be unique, but doesn't have to be secure.
+	//It's common to put it at the beginning of the ciphertext.
+	iv := cipherText[:aes.BlockSize]
+	cipherText = cipherText[aes.BlockSize:]
+
+	stream := cipher.NewCFBDecrypter(block, iv)
+	// XORKeyStream can work in-place if the two arguments are the same.
+	stream.XORKeyStream(cipherText, cipherText)
+
+	decoded_msg = string(cipherText)
+	return
+}
