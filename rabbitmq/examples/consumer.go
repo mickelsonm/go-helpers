f9f1806e42937b580938be157eb16d2a933c7eee
Matt Mickelson <mickelmr@gmail.com>
2015-02-17 12:17:04 -0600

    add rabbitmq example files

---
+++ rabbitmq/examples/consumer.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log"
+
+	"github.com/mickelsonm/go-helpers/rabbitmq"
+)
+
+func main() {
+	exchange := rabbitmq.Exchange{
+		Name:       "exchange",
+		RoutingKey: "hacker",
+	}
+	consumer, err := rabbitmq.NewConsumer("simple-consumer", "test-queue", exchange)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	for {
+		select {
+		case <-consumer.DoneChan:
+
+		}
+	}
+
+	consumer.Close()
+}
---
+++ rabbitmq/examples/producer.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+
+	"github.com/mickelsonm/go-helpers/rabbitmq"
+)
+
+func main() {
+	exchange := rabbitmq.Exchange{
+		Name:       "exchange",
+		RoutingKey: "hacker",
+	}
+
+	prod, err := rabbitmq.NewProducer(exchange)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	mess := "this is a really good test"
+	js, err := json.Marshal(mess)
+	if err != nil {
+		return
+	}
+
+	//message is just []byte...doesn't have to be json
+	if err = prod.SendMessage(js); err != nil {
+		log.Println(err)
+		return
+	}
+}
